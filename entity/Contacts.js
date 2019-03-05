acea570bea44c426250afd7a84261d7515389fbc
Richard Ekong <ekong.richard@yahoo.com>
2019-03-05 13:30:19 +0200

    Add entity Contacts.js

---
+++ entity/Contacts.js
@@ -0,0 +1,36 @@
+
+class Contacts{
+    setId (id){ 
+        this.id = id;
+    }
+
+    getId(){
+        return this.id;
+    }
+
+    setEmail (email){
+        this.email = email;
+    }
+
+    getEmail (){
+        return this.email;
+    }
+
+    setFirstName (firstName){
+        this.firstName = firstName
+    }
+
+    getFirstName (){
+        return this.firstName;
+    }
+
+    setLastName (lastName){
+        this.lastName = lastName;
+    }
+
+    getLastName (){
+        return this.lastName;
+    }
+
+}
+
