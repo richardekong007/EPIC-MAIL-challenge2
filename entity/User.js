8781d702bc9d18a81eeaadb1fb52bb54063f9c7c
Richard Ekong <ekong.richard@yahoo.com>
2019-03-05 13:21:08 +0200

    Add entity User

---
+++ entity/User.js
@@ -0,0 +1,39 @@
+
+export class User{
+    setId (id){
+        this.id = id;
+    }
+    getId (){
+        return this.id;
+    }
+
+    setEmail (email){
+        this.email = email;
+    }
+
+    getEmail (){
+        return this.email;
+    }
+
+    setFirstName (firstName){
+        this.firstName = firstName;
+    }
+
+    getFirstName (){
+        return this.firstName;
+    }
+
+    setLastName (){
+        this.lastName = lastName;
+    }
+
+    setPassword (password){
+        this.password = password;
+    }
+
+    getPassword (){
+        return this.password;
+    }
+
+}
+
