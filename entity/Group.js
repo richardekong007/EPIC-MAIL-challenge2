e8102b8511b2a71626d3396061381f14193e9edc
Richard Ekong <ekong.richard@yahoo.com>
2019-03-05 14:04:23 +0200

    Add entity Group

---
+++ entity/Group.js
@@ -0,0 +1,20 @@
+export class Group {
+    
+    setId (id){
+        this.id = id;
+    }
+
+    getId (){
+        return this.id;
+    }
+
+    setName (name){
+        this.name = name;
+    }
+
+    getName (){
+        return this.name;
+    }
+
+}
+
