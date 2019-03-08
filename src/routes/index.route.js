d5652ec4bdbdb928a31d5b641231f1db1c235f77
Richard Ekong <ekong.richard@yahoo.com>
2019-03-08 18:03:39 +0200

    Add Route for Creating/sending Messages by only authorized users

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import bodyParser from "body-parser";
 
-
 const app = express();
 const controller = require('../controllers/index.controller');
-
+const auth = require('../middleware/auth');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -13,6 +12,8 @@ app.use(bodyParser.json());
 app.post('/auth/signup', controller.signup);
 
 app.post('/auth/login', controller.login);
+
+app.post('/messages', auth,controller.sendMessage);
 
 export default app;
 
