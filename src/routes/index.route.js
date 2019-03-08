6c0b1277d10ce957321f41aee3e73001fdeb49d2
Richard Ekong <ekong.richard@yahoo.com>
2019-03-08 23:05:08 +0200

    Add api route for getting a specific email

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 
 const app = express();
 const controller = require('../controllers/index.controller');
-const auth = require('../middleware/auth');
+const authorize = require('../middleware/auth');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -13,7 +13,9 @@ app.post('/auth/signup', controller.signup);
 
 app.post('/auth/login', controller.login);
 
-app.post('/messages', auth,controller.sendMessage);
+app.post('/messages', authorize,controller.createEmail);
+
+app.post('/messages/:id', authorize, controller.getEmail);
 
 export default app;
 
