4ec3a92b8261252650867514f827aa338c44ed48
Richard Ekong <ekong.richard@yahoo.com>
2019-03-09 18:17:06 +0200

    Add route api route for deleting specific email

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -4,10 +4,11 @@ import bodyParser from "body-parser";
 const app = express();
 const controller = require('../controllers/index.controller');
 const authorize = require('../middleware/auth');
+const port = 8080;
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-
+app.set('port',port);
 
 app.post('/auth/signup', controller.signup);
 
@@ -18,6 +19,8 @@ app.post('/messages', authorize,controller.createEmail);
 app.get('/messages/:id', authorize, controller.getEmail);
 
 app.get('/messages', authorize,controller.getEmails);
+
+app.delete('/messages/:id', authorize, controller.deleteEmail);
 
 export default app;
 
