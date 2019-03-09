2e4aab326945778de201cd70aa129372cabe468d
Richard Ekong <ekong.richard@yahoo.com>
2019-03-09 18:28:20 +0200

    Modify listening operation of the server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import app from './routes/index.route';
 
-const port = 1000;
-const server = app.listen(process.env.PORT || port,()=>{
-    console.log(`Listening on port - ${port}`);
+//const port = 8080;
+const server = app;
+
+if (!module.parent)
+    server.listen(process.env.PORT || app.get('port'),()=>{
+    console.log(`Listening on port - ${app.get('port')}`);
 });
 
 export default server;
