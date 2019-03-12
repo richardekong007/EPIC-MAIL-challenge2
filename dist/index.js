0b1ed725628aeb644f1659c874e1414e649125dc
Richard Ekong <ekong.richard@yahoo.com>
2019-03-12 21:48:51 +0200

    Perform project build

--- dist/index.js
+++ dist/index.js
@@ -9,11 +9,10 @@ var _index = _interopRequireDefault(require("./routes/index.route"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var port = 1000;
-
-var server = _index.default.listen(process.env.PORT || port, function () {
-  console.log("Listening on port - ".concat(port));
+//const port = 8080;
+var server = _index.default;
+if (!module.parent) server.listen(process.env.PORT || _index.default.get('port'), function () {
+  console.log("Listening on port - ".concat(_index.default.get('port')));
 });
-
 var _default = server;
 exports.default = _default;
--- dist/routes/index.route.js
+++ dist/routes/index.route.js
@@ -9,21 +9,21 @@ var _express = _interopRequireDefault(require("express"));
 
 var _bodyParser = _interopRequireDefault(require("body-parser"));
 
+var _api = _interopRequireDefault(require("../api/api.v1"));
+
+var _api2 = _interopRequireDefault(require("../api/api.v2"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express.default)();
-
-var controller = require('../controllers/index.controller');
-
-var authorize = require('../middleware/auth');
-
+var port = 8080;
 app.use(_bodyParser.default.urlencoded({
   extended: false
 }));
 app.use(_bodyParser.default.json());
-app.post('/auth/signup', controller.signup);
-app.post('/auth/login', controller.login);
-app.post('/messages', authorize, controller.createEmail);
-app.post('/messages/:id', authorize, controller.getEmail);
+app.use('/v1', _api.default);
+app.use('/v2', _api2.default);
+app.use('/', _api2.default);
+app.set('port', port);
 var _default = app;
 exports.default = _default;
